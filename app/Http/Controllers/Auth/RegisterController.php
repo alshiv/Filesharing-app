f354258a493163e271a6f7f8e39bcdddeafac32b
Alexander Schigolev <schigolev@sibprofi.com>
2020-06-19 17:44:48 +0700

    add directory when user registated

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,12 +66,15 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'username' => $data['username'],
             'name' => $data['name'],
             'surname' => $data['surname'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        \Storage::makeDirectory($data['username']);
+        return $user;
     }
 }
