5c91e2d65eb961537fc30511f34b95df8efdbbb5
Alexander Schigolev <schigolev@sibprofi.com>
2020-06-26 13:01:04 +0700

    return to base user verification without crap

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -53,7 +53,6 @@
                             </div>
                         </div>
                         
-                        <!--
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
@@ -67,7 +66,6 @@
                                 @enderror
                             </div>
                         </div>
-                        -->
 
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
